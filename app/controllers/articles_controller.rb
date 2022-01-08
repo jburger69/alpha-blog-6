57153aa882060b7e0a22e696cd108c374cbf8848
Joshua Burger <joshberger6969@outlook.com>
2022-01-07 21:33:09 -0500

    Create 2 new actions new and create for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,27 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        # byebug
+        if @article.valid?
+            @article.save
+            redirect_to @article
+        else
+            redirect_to new_article_path
+        end
+    end
+
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
     
 end
